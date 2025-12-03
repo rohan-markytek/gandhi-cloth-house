6b23fbd4316511191e7c9ba80c74d93332567483
arpanray <ron@markytek.com>
2025-12-03 17:00:31 +0530

    Name change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="w-full bg-white shadow-inner py-4 px-6 text-center text-gray-500 text-sm fixed bottom-0 left-0">
-      <p>© {new Date().getFullYear()} Gandhi Cloth House</p>
+      <p>© {new Date().getFullYear()} Gandhi Cloth Store</p>
     </footer>
   );
 }
