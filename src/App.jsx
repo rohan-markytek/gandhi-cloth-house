fc23b298d289063f0471a21d6dc62e0b5c5e5b38
arpanray <arpan@markytek.com>
2026-04-21 01:07:18 +0530

    issue fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
